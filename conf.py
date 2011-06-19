e55cec2ee4bb21723ad4218bd68b49db3af96469
Volodymyr Hotsyk <gotsyk@gmail.com>
2011-06-20 00:54:38 +0300

    Documentation update to use small theme

--- conf.py
+++ conf.py
@@ -95,7 +95,7 @@ pygments_style = 'sphinx'
 # Sphinx are currently 'default' and 'sphinxdoc'.
 sys.path.append(os.path.abspath('_themes'))
 html_theme_path = ['_themes']
-html_theme = 'flask'
+html_theme = 'flask_small'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
