c2c0c83ca58e850c07d1aabd3fcb0ab3a4dc6e34
Anton Strogonoff <anton@strogonoff.name>
2015-05-21 18:18:52 +0300

    Marked version as 1.0-pre1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ version = __import__('flaskext').get_version()
 
 setup(
     name='Flask-Upwork',
-    version='1.0',
+    version='1.0-pre1',
     download_url='http://github.com/strogonoff/flask-upwork',
     url='https://github.com/strogonoff/flask-upwork',
     license='BSD',
